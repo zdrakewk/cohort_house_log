5e139e4ec969f74accd5e594e33940ae0bb65695
github username <github email address>
2020-05-05 19:43:45 -0500

    Tuesday study group

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,4 +7,42 @@ class RoomsController < ApplicationController
     # binding.pry
     erb :'rooms/index' # render the index view
   end
+
+  get '/rooms/new' do # display form to create a new room obj
+    # binding.pry
+    erb :'rooms/new'
+  end
+
+  post '/rooms' do # create a new room obj based on form (user) input
+    # option 1
+    # @new_room = Room.new
+    # @new_room.name = params[:name]
+    # @new_room.theme = params[:theme]
+    # @new_room.use = params[:use]
+
+  # option 2
+  # @new_room = Room.new(
+  #     name: params[:name],
+  #     theme: params[:theme],
+  #     use: params[:use]
+  #   )
+
+  # option 3
+    @new_room = Room.new(params)
+
+    @new_room.save
+
+    redirect "/rooms/#{@new_room.id}"
+  end
+
+  get '/rooms/:id' do # SHOW route
+    # display ONE room obj
+    # binding.pry
+    @room_obj = Room.find_by(id: params[:id])
+    # @room_obj = Room.find( params[:id])
+
+    erb :'rooms/show.html'
+  end
+
+
 end
